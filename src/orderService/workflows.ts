a864d4fd94a85f3a2788b0d61407b40a9d49bac7
Yamen Sader <yamen.sader@gmail.com>
2023-01-16 17:01:37 +1100

    Simplified xstate sleep adapter for temporal

--- src/orderService/workflows.ts
+++ src/orderService/workflows.ts
@@ -103,105 +103,95 @@ const setStatus = (status: string) =>
   });
 
 function createOrderMachine(orderId: string) {
-  return createMachine<OrderMachineContext, OrderMachineEvents>(
-    {
-      id: 'orderMachine',
-      initial: 'created',
-      predictableActionArguments: true,
-      context: {
-        orderId,
-        status: 'CREATING',
-        paymentInfo: null,
-        paymentId: null
-      },
-      states: {
-        created: {
-          invoke: {
-            src: 'createOrderIntent',
-            onDone: 'pending',
-            onError: 'createFailed'
-          }
-        },
-        pending: {
-          entry: setStatus('PENDING'),
-          on: {
-            SUBMIT_PAYMENT: {
-              actions: assign({
-                status: (_context, _data) => 'SUBMITTING_ORDER',
-                paymentInfo: (_context, event) => event.paymentInfo
-              }),
-              target: 'submitOrder'
-            }
-          },
-          after: {
-            10000: 'orderTimeout'
-          }
-        },
-        submitOrder: {
-          invoke: {
-            src: 'submitOrder',
-            onDone: {
-              target: 'orderSubmitted',
-              actions: assign({
-                status: (_context, _data) => 'ORDER_SUBMITTED',
-                paymentId: (_context, event) => event.data
-              })
-            },
-            onError: 'orderFailed'
-          }
-        },
-        orderSubmitted: {
-          initial: 'wait',
-          on: {
-            PAYMENT_RESULT: [{ target: 'paymentSuccess', cond: (context, event) => event.paymentSuccess }, { target: 'paymentFailed' }]
-          },
-          states: {
-            wait: {
-              after: {
-                2000: 'checkPaymentStatus'
-              }
-            },
-            checkPaymentStatus: {
-              //entry: raise({ type: 'PAYMENT_RESULT', paymentSuccess: true })
-              invoke: {
-                src: 'getPaymentStatus'
-              },
-              on: {
-                PAYMENT_PENDING: 'wait'
-              }
-            }
-          }
-        },
-        paymentSuccess: {
-          entry: setStatus('PAYMENT_SUCCESS'),
-          type: 'final'
-        },
-        paymentFailed: {
-          entry: setStatus('PAYMENT_FAILED'),
-          type: 'final'
-        },
-        orderFailed: {
-          entry: setStatus('ORDER_FAILED'),
-          type: 'final'
-        },
-        orderTimeout: {
-          entry: setStatus('ORDER_TIMEOUT'),
-          type: 'final'
-        },
-        createFailed: {
-          entry: setStatus('CREATE_FAILED'),
-          type: 'final'
-        }
-      }
+  return createMachine<OrderMachineContext, OrderMachineEvents>({
+    id: 'orderMachine',
+    initial: 'created',
+    predictableActionArguments: true,
+    context: {
+      orderId,
+      status: 'CREATING',
+      paymentInfo: null,
+      paymentId: null
     },
-    {
-      services: {
-        createOrderIntent: async (context) => {
-          return await createOrderIntent(context.orderId);
+    states: {
+      created: {
+        invoke: {
+          src: 'createOrderIntent',
+          onDone: 'pending',
+          onError: 'createFailed'
         }
+      },
+      pending: {
+        entry: setStatus('PENDING'),
+        on: {
+          SUBMIT_PAYMENT: {
+            actions: assign({
+              status: (_context, _data) => 'SUBMITTING_ORDER',
+              paymentInfo: (_context, event) => event.paymentInfo
+            }),
+            target: 'submitOrder'
+          }
+        },
+        after: {
+          10000: 'orderTimeout'
+        }
+      },
+      submitOrder: {
+        invoke: {
+          src: 'submitOrder',
+          onDone: {
+            target: 'orderSubmitted',
+            actions: assign({
+              status: (_context, _data) => 'ORDER_SUBMITTED',
+              paymentId: (_context, event) => event.data
+            })
+          },
+          onError: 'orderFailed'
+        }
+      },
+      orderSubmitted: {
+        initial: 'wait',
+        on: {
+          PAYMENT_RESULT: [{ target: 'paymentSuccess', cond: (context, event) => event.paymentSuccess }, { target: 'paymentFailed' }]
+        },
+        states: {
+          wait: {
+            after: {
+              2000: 'checkPaymentStatus'
+            }
+          },
+          checkPaymentStatus: {
+            invoke: {
+              src: 'getPaymentStatus'
+            },
+            on: {
+              PAYMENT_PENDING: 'wait'
+            }
+          }
+        }
+      },
+      paymentSuccess: {
+        entry: setStatus('PAYMENT_SUCCESS'),
+        type: 'final'
+      },
+      paymentFailed: {
+        entry: setStatus('PAYMENT_FAILED'),
+        type: 'final'
+      },
+      orderFailed: {
+        entry: setStatus('ORDER_FAILED'),
+        type: 'final'
+      },
+      orderTimeout: {
+        entry: setStatus('ORDER_TIMEOUT'),
+        type: 'final'
+      },
+      createFailed: {
+        entry: setStatus('CREATE_FAILED'),
+        type: 'final'
       }
     }
-  );
+  });
 }
 
 export async function orderX(orderId: string) {
--- src/orderService/xstate.ts
+++ src/orderService/xstate.ts
@@ -1,22 +1,17 @@
 import { CancellationScope, isCancellation, sleep } from '@temporalio/workflow';
-import { EventObject, interpret, InterpreterFrom, StateMachine, StateSchema } from 'xstate';
+import { EventObject, interpret, StateMachine, StateSchema } from 'xstate';
 
 export function interpretWithTemporal<T1, T2 extends StateSchema<any>, T3 extends EventObject>(machine: StateMachine<T1, T2, T3>) {
   const service = interpret(machine, {
     clock: {
       setTimeout(fn, timeout) {
-        const scope = {
-          cancellationScope: new CancellationScope(),
-          slept: false
-        };
+        const scope = new CancellationScope();
 
-        scope.cancellationScope
-          .run(() => {
-            return sleep(timeout).then((args) => {
-              scope.slept = true;
-              fn(args);
-            });
-          })
+        scope
+          // cancellation scope only matters for the sleep call
+          .run(() => sleep(timeout))
+          // continuation is not cancelled once sleep is done
+          .then(fn)
           .catch((err) => {
             if (isCancellation(err)) {
               return;
@@ -27,10 +22,8 @@ export function interpretWithTemporal<T1, T2 extends StateSchema<any>, T3 extend
         return scope;
       },
 
-      clearTimeout(scope: { cancellationScope: CancellationScope; slept: boolean }) {
-        if (!scope.slept) {
-          scope.cancellationScope.cancel();
-        }
+      clearTimeout(scope: CancellationScope) {
+        scope.cancel();
       }
     }
   });
